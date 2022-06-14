aa2f597104fd41603c8bd6d61cd07615fb12e5f1
Zy5879 <ZaireM21@gmail.com>
2022-06-14 13:39:59 -0400

    created a function for basic math operations

---
+++ script.js
@@ -0,0 +1,19 @@
+const add = function(a,b) {
+    let sum = a + b;
+    return sum;
+}
+
+const subtract = function (c,d) {
+    let diff = c - d;
+    return diff;
+}
+
+const multiply = function(e,f) {
+    let times = e * f;
+    return times;
+}
+
+const divide = function(g,h) {
+    let divis = g / h;
+    return divis;
+}
