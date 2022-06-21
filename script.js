915acccb1c775524ac880ebfaa09e6540ed10d1e
Zy5879 <ZaireM21@gmail.com>
2022-06-20 21:26:35 -0400

    numbers and decimal pop in the display

--- script.js
+++ script.js
@@ -8,6 +8,25 @@ keys.addEventListener('click', e => {
         const action = key.dataset.action
         const keyContent = key.textContent
         const displayedNum = display.textContent
+        if(!action) {
+            if(displayedNum === '0') {
+                display.textContent = keyContent
+            } else {
+                display.textContent = displayedNum + keyContent
+            }
         
+        }
+    }
+})
+
+keys.addEventListener('click', e => {
+    if(e.target.matches('button')) {
+        const key = e.target
+        const action = key.dataset.action
+        const keyContent = key.textContent
+        const displayedNum = display.textContent
+        if(action === 'decimal') {
+            display.textContent = displayedNum + '.'
+        }
     }
 })
