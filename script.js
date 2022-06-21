7e42e4b66de55adab49b6a9c22b9e65c0b823691
Zy5879 <ZaireM21@gmail.com>
2022-06-20 21:06:27 -0400

    beginning to add functionality to buttons

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+//get values in display
+const display = document.querySelector('.display')
+const keys = document.querySelector('.calcKeys')
 
+keys.addEventListener('click', e => {
+    if(e.target.matches('button')) {
+        const key = e.target
+        const action = key.dataset.action
+        const keyContent = key.textContent
+        const displayedNum = display.textContent
+        
+    }
+})
