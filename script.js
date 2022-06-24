2a21a8a3a20f7b6552718b22f6b997396c8b9c77
Zy5879 <ZaireM21@gmail.com>
2022-06-24 17:15:30 -0400

    numbers pop up in display

--- script.js
+++ script.js
@@ -9,3 +9,16 @@ const numbers = document.querySelectorAll('.number')
 const clear = document.querySelector('.clear')
 
 const equal = document.querySelector('.equal')
+
+numbers.forEach(btn => {
+    btn.addEventListener('click', e => {
+        handleNum(e.target.textContent)
+    });
+});
+
+function handleNum(number) {
+    if(currentNum.length <= 12) {
+    currentNum += number;
+    currentNumber.textContent = currentNum;
+    }
+}
