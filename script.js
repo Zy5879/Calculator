81b79512e8b0e53b879ca736c9502667169ac2dd
Zy5879 <ZaireM21@gmail.com>
2022-06-27 16:19:13 -0400

    converted all clicked values from string to array

--- script.js
+++ script.js
@@ -7,22 +7,23 @@ keys.forEach(button => {
         let value = e.target.dataset.value
         //whenever you click equal just the returns number
         if(value === " ") {
-            //
+            //changing the innerText from strings to arrays.
+            //allowing the numbers to be properly solved
             if(input.innerText.includes('/')) {
                 const checkArr = input.innerText.split('/')
-                operate('/', checkArr[0], checkArr[1])
+                operation('/', checkArr[0], checkArr[1])
             }
             if(input.innerText.includes('+')) {
                 const checkArr = input.innerText.split('+')
-                operate('+', checkArr[0], checkArr[1])
+                operation('+', checkArr[0], checkArr[1])
             }
             if(input.innerText.includes('x')) {
                 const checkArr = input.innerText.split('x')
-                operate('x', checkArr[0], checkArr[1])
+                operation('x', checkArr[0], checkArr[1])
             }
             if(input.innerText.includes('-')) {
                 const checkArr = input.innerText.split('-')
-                operate('-', checkArr[0], checkArr[1])
+                operation('-', checkArr[0], checkArr[1])
             }
         }
         input.innerText += value
@@ -33,7 +34,7 @@ keys.forEach(button => {
     })
 })
 //functions that solve math equations
-const operate = function(operator, n1, n2) {
+const operation = function(operator, n1, n2) {
     const num1 = Number(n1)
     const num2 = Number(n2)
     if(operator ===  '/') {
