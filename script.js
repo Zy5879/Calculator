840a7d628fed531bef8287d674127893c4777dc7
Zy5879 <ZaireM21@gmail.com>
2022-07-01 13:51:51 -0400

    whenever you click operator, solution displays

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
+let tempArray  = [];
+// let tempNum2 = [];
+  
 const input = document.querySelector('.display')
-const keys = document.querySelectorAll('.button')
+const keys = document.querySelectorAll('.buttons')
+// const equals = document.querySelector('.equal')
 
 //numbers popup in display
 keys.forEach(button => {
     button.addEventListener('click', e => {
         let value = e.target.dataset.value
         //whenever you click equal just the returns number
-        if(value === " ") {
+         if(
+            value === " " ||
+            value === "x" ||
+            value === "/" ||
+            value === "+" ||
+            value === "-"
+            ) {
             //changing the innerText from strings to arrays.
             //allowing the numbers to be properly solved
             if(input.innerText.includes('/')) {
@@ -26,34 +36,48 @@ keys.forEach(button => {
                 operate('-', checkArr[0], checkArr[1])
             }
         }
-        input.innerText += value
-
-       if(value === 'clear') {
-        input.innerText = '';
-       }
+        //allows you to create string of numbers
+         input.innerText += value
+        
+        //clears display
+        if(value === 'clear') {
+         input.innerText = '';
+        }
+    
     })
 })
+
 //functions that solve math equations
 const operate = function(operator, n1, n2) {
     const num1 = Number(n1)
     const num2 = Number(n2)
     if(operator ===  '/') {
-        input.innerText = num1 / num2
-        console.log(num1 / num2)
+        finalValue = num1 / num2
+        input.innerText = finalValue
+    //  return num1 / num2
     }
     if(operator ===  'x') {
-        input.innerText = num1 * num2
-        console.log(num1 * num2)
+        finalValue = num1 * num2
+        input.innerText = finalValue
+        // console.log(num1 * num2)
+        // return num1 * num2
     }
     if(operator ===  '-') {
-        input.innerText = num1 - num2
-        console.log(num1 - num2)
+        finalValue = num1 - num2
+        input.innerText = finalValue;
+        // console.log(num1 - num2)
+        // return num1 - num2
     }
     if(operator ===  '+') {
-        input.innerText = num1 + num2
-        console.log(num1 + num2)
+        finalValue = num1 + num2
+        input.innerText = finalValue;
+        // console.log(num1 + num2)
+        // return num1 + num2
     }
+
 }
+
+
 
 
 // function clearDisplay() {
